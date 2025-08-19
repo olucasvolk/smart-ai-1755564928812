f3389bdbad3c717d82e146ddb71cf1c6bb0d1dc0
Lucas William <107009649+olucasvolk@users.noreply.github.com>
2025-08-18 21:55:35 -0300

    Add app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+
+export default function Portfolio() {
+  return (
+    <Layout>
+      <div className='container mx-auto mt-10'>
+        <h1 className='text-3xl font-bold mb-4'>Meu Portfólio</h1>
+        <p className='text-lg'>Bem-vindo ao meu portfólio de desenvolvedor!</p>
+      </div>
+    </Layout>
+  );
+}
